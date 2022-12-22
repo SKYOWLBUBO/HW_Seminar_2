8ea1a3bb1a2f1cf21dffd7d780cf70cdbedead99
Matvey Firsov <firsov.mat@yandex.ru>
2022-12-22 09:27:41 +0300

    Add files

---
+++ Task10/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+} 
+bool ValidateNumbe(int number)
+{
+    if(number>100 || number<1000)
+    {
+        Console.WriteLine("это число трехзначное");
+        return true;
+    }
+  return false;
+}
+
+
+int number = Prompt("Введите трехзначное число");
+if(ValidateNumbe(number))
+{
+    int MiddleDigit = number / 10 % 10;
+    System.Console.WriteLine($"Вторая цифра числа {number} равна {MiddleDigit}");
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ Task15/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите число соответствующее дню недели");
+string str = Console.ReadLine();
+int day = Convert.ToInt32(str);
+
+if(day == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if(day == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if(day == 3)
+{
+    Console.WriteLine("Среда");
+}
+if(day == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if(day == 5)
+{
+    Console.WriteLine("Пятница,завтра выходной");
+}
+if(day == 6)
+{
+    Console.WriteLine("Суббота,Выходной");
+}
+if(day == 7)
+{
+    Console.WriteLine("Воскресенье,Выходной");
+}
